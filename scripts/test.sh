461157a14044eef03fd07a75709d59c041b7762d
Lightning K0ala <lightning.k0ala@protonmail.com>
2024-03-15 18:13:46 +0000

    relative script reference in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,16 @@
 # This script is used to test the Voltage CLI with
 # environment variable overrides.
 
+# Get the directory of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Set environment variables from .env
-source .env
+source "$SCRIPT_DIR/../.env"
 
 # Export all variables
-for var in $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/') ; do
+for var in $(grep -v '^#' "$SCRIPT_DIR/../.env" | sed -E 's/(.*)=.*/\1/') ; do
     export "$var"
 done
 
 # Run the command to create a Voltage application
-npx create-voltage-app
+node "$SCRIPT_DIR/../src/index.js"
