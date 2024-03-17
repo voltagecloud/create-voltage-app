0997d5084ca9ac1a82d6e2f6cdfbca522fdf36bd
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-03-17 00:21:53 -0400

    add eslint

--- apps/sveltekit-kitchen-sink/app/src/components/QrCode/QrCode.svelte
+++ apps/sveltekit-kitchen-sink/app/src/components/QrCode/QrCode.svelte
@@ -1,60 +1,63 @@
 <script lang="ts">
-    import { browser } from "$app/environment";
-    import { onDestroy, onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { onDestroy, onMount } from 'svelte';
 
-    export let parameters = '';
-    export let address = '';
-    export let invoice = '';
-    export let imgSrc = './assets/voltage.svg';
-    export let moduleColor = '#ff5000';
-    export let positionCenterColor = '#cc4100';
-    export let positionRingColor = '#9f3200';
-    export let isPolling = false;
-    export let interval = 3000 // Interval to poll in milliseconds
-    export let pollCallback: () => void | null = null ;
+	export let parameters = '';
+	export let address = '';
+	export let invoice = '';
+	export let imgSrc = './assets/voltage.svg';
+	export let moduleColor = '#ff5000';
+	export let positionCenterColor = '#cc4100';
+	export let positionRingColor = '#9f3200';
+	export let isPolling = false;
+	export let interval = 3000; // Interval to poll in milliseconds
+	export let pollCallback = () => {}; // FIXME: allow undefined without breaking TS
 
-    let rendered: boolean = false;
+	let rendered: boolean = false;
 
-    onMount(() => {
-        const qr = document.getElementById('qr') as any;
-        if (qr) {
-            qr.addEventListener('codeRendered', () => {
-                qr.callback = pollCallback;
-                qr.animateQRCode('MaterializeIn')
-                console.log({rendered})
-                rendered = true
-            });
-        }
-    })
+	onMount(() => {
+		const qr = document.getElementById('qr') as any;
+		if (qr) {
+			qr.addEventListener('codeRendered', () => {
+				qr.callback = pollCallback;
+				qr.animateQRCode('MaterializeIn');
+				console.log({ rendered });
+				rendered = true;
+			});
+		}
+	});
 
-    onDestroy(() => {
-        const qr = document.getElementById('qr') as any;
-        if (qr) {
-            qr.removeEventListener('codeRendered', () => {
-                qr.callback = undefined;
-                rendered = false;
-            });
-        }
-    })
+	onDestroy(() => {
+		const qr = document.getElementById('qr') as any;
+		if (qr) {
+			qr.removeEventListener('codeRendered', () => {
+				qr.callback = undefined;
+				rendered = false;
+			});
+		}
+	});
 </script>
 
 <svelte:head>
-    <script type="module" src="https://unpkg.com/bitcoin-qr@1.0.0/dist/bitcoin-qr/bitcoin-qr.esm.js"/>
-    <script src="https://unpkg.com/@bitjson/qr-code@1.0.2/dist/qr-code.js" />
+	<script
+		type="module"
+		src="https://unpkg.com/bitcoin-qr@1.0.0/dist/bitcoin-qr/bitcoin-qr.esm.js"
+	></script>
+	<script src="https://unpkg.com/@bitjson/qr-code@1.0.2/dist/qr-code.js"></script>
 </svelte:head>
 
 {#if browser}
-    <bitcoin-qr
-        id="qr"
-        class="w-1/3"
-        bitcoin={address}
-        lightning={invoice}
-        {parameters}
-        module-color={moduleColor}
-        position-center-color={positionCenterColor}
-        position-ring-color={positionRingColor}
-        img-src={imgSrc}
-        is-polling={isPolling}
-        {interval}
-    />
+	<bitcoin-qr
+		id="qr"
+		class="w-1/3"
+		bitcoin={address}
+		lightning={invoice}
+		{parameters}
+		module-color={moduleColor}
+		position-center-color={positionCenterColor}
+		position-ring-color={positionRingColor}
+		img-src={imgSrc}
+		is-polling={isPolling}
+		{interval}
+	/>
 {/if}
