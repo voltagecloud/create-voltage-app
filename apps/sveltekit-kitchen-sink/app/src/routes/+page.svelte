eb92a2cbf394c74a90b8dbbcf6b62dae40cce8ad
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-03-17 00:49:09 -0400

    restyle

--- apps/sveltekit-kitchen-sink/app/src/routes/+page.svelte
+++ apps/sveltekit-kitchen-sink/app/src/routes/+page.svelte
@@ -1,125 +1,131 @@
 <script lang="ts">
-  import {
-    lndGetWalletBalance,
-    lndGetInfo,
-    lndListChannels,
-    lndListInvoices,
-    lndCreateInvoice,
-    lndNewAddress,
-    type GetInfoResponse
-  } from "$lib/lnd";
-  import { onMount } from "svelte";
-  import QrCode from "../components/QrCode/QrCode.svelte";
+	import {
+		lndGetWalletBalance,
+		lndGetInfo,
+		lndListChannels,
+		lndListInvoices,
+		lndCreateInvoice,
+		lndNewAddress,
+		type GetInfoResponse
+	} from '$lib/lnd';
+	import { onMount } from 'svelte';
+	import QrCode from '$components/QrCode/QrCode.svelte';
+	import Button from '$components/Button/Button.svelte';
 
-  let amount = 0;
-  let memo = "";
-  let invoice = "";
-  let address = "";
-  let info: GetInfoResponse;
+	let amount = 0;
+	let memo = '';
+	let invoice = '';
+	let address = '';
+	let info: GetInfoResponse;
 
-  const getNewAddress = async () => {
-    address = (await lndNewAddress()).address;
-  };
+	const getNewAddress = async () => {
+		address = (await lndNewAddress()).address;
+	};
 
-  const createInvoice = async () => {
-    invoice = (await lndCreateInvoice(amount, memo)).payment_request;
-  };
+	const createInvoice = async () => {
+		invoice = (await lndCreateInvoice(amount, memo)).payment_request;
+	};
 
-  const getInfo = async () => {
-     info = await lndGetInfo();
-  };
+	const getInfo = async () => {
+		info = await lndGetInfo();
+	};
 
-  onMount(async () => {
-    await getInfo();
-  })
+	onMount(async () => {
+		await getInfo();
+	});
 </script>
 
-<img src="/assets/voltage-name.svg" alt="Voltage" class="w-1/5" />
-<h1>⚡️ My Voltage App ⚡️</h1>
-<!-- TODO: voltage documentation -->
+<div class="mx-8 flex flex-col items-center">
+	<img src="/assets/voltage-name.svg" alt="Voltage" class="w-1/5" />
+	<!-- TODO: voltage documentation -->
 
-<h2>Create Invoice</h2>
-<!-- Make a simple form to call the lndCreateInvoices function with the amount and memo -->
-<form on:submit|preventDefault={createInvoice}>
-  <label for="amount">Amount</label>
-  <input type="number" id="amount" bind:value={amount} />
-  <label for="memo">Memo</label>
-  <input type="text" id="memo" bind:value={memo} />
-  <button type="submit">Create Invoice</button>
-</form>
+	<div class="flex">
+		<div class="max-w-1/2 flex-1">
+			{#if info?.chains?.length}
+				<p>Network: {info.chains[0].network}</p>
+			{/if}
 
-<!-- Call the lndNewAddress function and show QrCode -->
-<form on:submit|preventDefault={getNewAddress}>
-  <button type="submit">Get New Address</button>
-</form>
+			<h2>Node Balance</h2>
+			{#await lndGetWalletBalance()}
+				<p>loading...</p>
+			{:then result}
+				<ul>
+					<li>Total Balance: {result.total_balance}</li>
+					<li>Confirmed Balance: {result.confirmed_balance}</li>
+					<li>Unconfirmed Balance: {result.unconfirmed_balance}</li>
+				</ul>
+			{:catch error}
+				<p>error: {error.message}</p>
+			{/await}
 
-{#if address || invoice}
-  <QrCode {address} {invoice} />
-{/if}
+			<h2>Node Info</h2>
+			{#await lndGetInfo()}
+				<p>loading...</p>
+			{:then result}
+				<ul>
+					<li>Node Alias: {result.alias}</li>
+					<li>Pubkey: {result.identity_pubkey}</li>
+					<li>Peers: {result.num_peers}</li>
+					<li>Synced to Chain: {result.synced_to_chain}</li>
+				</ul>
+			{:catch error}
+				<p>error: {error.message}</p>
+			{/await}
 
-{#if info?.chains?.length}
-  <p>Network: {info.chains[0].network}</p>
-{/if}
+			<h2>Channel Info</h2>
+			{#await lndListChannels()}
+				<p>loading...</p>
+			{:then result}
+				<ul>
+					<li>Number of channels: {result.channels.length}</li>
+				</ul>
+			{:catch error}
+				<p>error: {error.message}</p>
+			{/await}
 
-<h2>Node Balance</h2>
-{#await lndGetWalletBalance()}
-  <p>loading...</p>
-{:then result}
-  <ul>
-    <li>Total Balance: {result.total_balance}</li>
-    <li>Confirmed Balance: {result.confirmed_balance}</li>
-    <li>Unconfirmed Balance: {result.unconfirmed_balance}</li>
-  </ul>
-{:catch error}
-  <p>error: {error.message}</p>
-{/await}
+			<h2>Channel Info</h2>
+			{#await lndListChannels()}
+				<p>loading...</p>
+			{:then result}
+				<ul>
+					<li>Number of channels: {result.channels.length}</li>
+				</ul>
+			{:catch error}
+				<p>error: {error.message}</p>
+			{/await}
 
-<h2>Node Info</h2>
-{#await lndGetInfo()}
-  <p>loading...</p>
-{:then result}
-  <ul>
-    <li>Node Alias: {result.alias}</li>
-    <li>Pubkey: {result.identity_pubkey}</li>
-    <li>Peers: {result.num_peers}</li>
-    <li>Synced to Chain: {result.synced_to_chain}</li>
-  </ul>
-{:catch error}
-  <p>error: {error.message}</p>
-{/await}
+			<h2>Invoices</h2>
+			{#await lndListInvoices()}
+				<p>leading...</p>
+			{:then result}
+				<ul class=" max-w-full break-all">
+					<!-- catch a dispatched created invoice -->
 
-<h2>Channel Info</h2>
-{#await lndListChannels()}
-  <p>loading...</p>
-{:then result}
-  <ul>
-    <li>Number of channels: {result.channels.length}</li>
-  </ul>
-{:catch error}
-  <p>error: {error.message}</p>
-{/await}
+					{#each result.invoices as invoice}
+						<li>{invoice.add_index} - {invoice.state} {invoice.payment_request}</li>
+					{/each}
+				</ul>
+			{/await}
+		</div>
+		<div class="flex flex-1 flex-col gap-4 bg-green-100">
+			<h2>Create Invoice</h2>
+			<!-- Make a simple form to call the lndCreateInvoices function with the amount and memo -->
+			<form on:submit|preventDefault={createInvoice}>
+				<label for="amount">Amount</label>
+				<input type="number" id="amount" bind:value={amount} />
+				<label for="memo">Memo</label>
+				<input type="text" id="memo" bind:value={memo} />
+				<Button type="submit">Create Invoice</Button>
+			</form>
 
-<h2>Channel Info</h2>
-{#await lndListChannels()}
-  <p>loading...</p>
-{:then result}
-  <ul>
-    <li>Number of channels: {result.channels.length}</li>
-  </ul>
-{:catch error}
-  <p>error: {error.message}</p>
-{/await}
-
-<h2>Invoices</h2>
-{#await lndListInvoices()}
-  <p>leading...</p>
-{:then result}
-  <ul>
-    <!-- catch a dispatched created invoice -->
-
-    {#each result.invoices as invoice}
-      <li>{invoice.add_index} - {invoice.state} {invoice.payment_request}</li>
-    {/each}
-  </ul>
-{/await}
-
+			<!-- Call the lndNewAddress function and show QrCode -->
+			<form on:submit|preventDefault={getNewAddress}>
+				<Button type="submit">Get New Address</Button>
+			</form>
+			{#if address || invoice}
+				<!-- TODO: check if payment succeeded -->
+				<QrCode {address} {invoice} />
+			{/if}
+		</div>
+	</div>
+</div>
